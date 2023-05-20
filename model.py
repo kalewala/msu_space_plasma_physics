e67ce3a72c7e5d49b4d1e5dace001ceead4f80a0
El'dar Garipov <e_garipow@mail.ru>
2023-05-20 22:12:07 +0300

    Исправлено интегрирование

--- model.py
+++ model.py
@@ -4,75 +4,85 @@
 Python 3.10.7"""
 
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 
 t = 0  # начальное состояние - нулевое время
-dt = 1  # шаг по времени
-N = 10  # количество шагов
+T = 0.05  # конечное время
+N = 1000  # количество шагов интегрирования
 
-xi = np.array([0.0000000001, 0.0000000004, 0.0])  # начальные координаты частицы i(x, y, z)
-vi = np.array([0.0000000002, 0.0000000001, 0.0])  # начальные векторы скоростей i(vx, vy, vz)
-qi = -1.602_176_634e-19  # заряд частицы i
-mi = 9.109_383_7015e-31  # масса электрона
+# частицы
+r1 = [0.0, 0.0, 0.0]  # начальные координаты в м
+r2 = [0.0001, 0.0001, 0.0001]
+r1 = np.array(r1, dtype="float64")  # перевод в np.array
+r2 = np.array(r2, dtype="float64")
 
-xj = np.array([0.000000001, 0.0000000007, 0.000000001])  # начальные координаты частицы j(x, y, z)
-vj = np.array([-0.0000000002, -0.0000000001, 0.0])  # начальные векторы скоростей j(vx, vy, vz)
-qj = 1.602_176_634e-19  # заряд цастицы j
-mj = 9.109_383_7015e-31  # масса эектрона
+v1 = [0.002, 0.001, 0.003]  # начальная скорость в м/с
+v2 = [-0.003, -0.003, -0.001]
+v1 = np.array(v1, dtype="float64")  # перевод в np.array
+v2 = np.array(v2, dtype="float64")
+
+s1 = np.array([[r1[0]],
+               [r1[1]],
+               [r1[2]]], 'float64')  # пройденный путь по координатам
+s2 = np.array([[r2[0]],
+               [r2[1]],
+               [r2[2]]], 'float64')
+
+m1 = 9.109_383_7015e-31  # масса электрона в кг
+m2 = 9.109_383_7015e-31
+
+q1 = -1.602_176_634e-19  # заряд электрона в Кл
+q2 = -1.602_176_634e-19
 
 c = 299_792_458  # скорость света по СИ (м/с)
 m0 = 4 * np.pi * 10**(-7)  # магнитная постоянная (Гн/м)
 e0 = 1/(m0 * c**2)  # электричекая постоянная
 k = 1/(4 * np.pi * e0)  # диэлектрическая проницаемость среды
 
-dx = xj - xi  # векторная разность положения частиц
-r = (dx[0]**2 + dx[1]**2 + dx[2]**2)**0.5  # расстояние между частицами
+def Equations(params, t, k, q1, q2):
+    r1 = params[0:3]  # координаты частицы
+    r2 = params[3:6]
+    v1 = params[6:9]  # вектор скорости частицы
+    v2 = params[9:12]
 
-def Fij(r):
-    F = (qi*qj * (r))/(4 * np.pi * e0 * np.absolute(r)**3)  # закон Кулона
-    return F
+    r12 = np.linalg.norm(r2 - r1)  # расстояние между частицами
+    dv1 = k*q2*(r2 - r1) / r12**3  # вектор приращения скорости
+    dv2 = k*q1*(r1 - r2) / r12**3
+    dr1 = v1  # вектор приращения пути
+    dr2 = v2
 
-def Fv(r, v):
-    F = k * (qi * qj * v)/(r**2 * r)
-    return F
-
-
-Xi = np.empty([N, 3])  # матрица значений частицы i
-Xj = np.empty([N, 3])  # матрица значений частицы j
-Xi[0] = xi
-Xj[0] = xj
-for i in range(1, N):
-    dx = xj - xi  # векторная разность положения частиц
-    r = (dx[0]**2 + dx[1]**2 + dx[2]**2)**0.5  # расстояние между частицами
-    
-    F = np.array([0.0, 0.0, 0.0])
-    for n in range(3):
-        F[n] = Fv(r, dx[n])
-
-    vi += F
-    xi += vi
-    Xi[i] = xi
-
-    vj -= F
-    xj += vj
-    Xj[i] = xj
+    r_derivs = np.concatenate((dr1, dr2))
+    v_derivs = np.concatenate((dv1, dv2))
+    derivs = np.concatenate((r_derivs, v_derivs))
+    return derivs
 
 
+params = np.array([r1, r2, v1, v2])
+params = params.flatten()
+time_arr = np.linspace(0, 0.05, 1001)  # шаги по времени: старт, стоп, количество шагов
+sol = scipy.integrate.odeint(Equations,  # уравнения движения частиц
+                           params,  # основные параметры (координаты и скорости)
+                           time_arr,  # шаги по времени
+                           args=(k, q1, q2))  # дополнительные параметры
+r1_sol = sol[:,:3]
+r2_sol = sol[:,3:6]
 
-x1 = np.array([x[0] for x in Xi])
-y1 = np.array([x[1] for x in Xi])
-z1 = np.array([x[2] for x in Xi])
+# график
+fig = plt.figure(figsize=(7, 7))  # размер полотна
+ax = fig.add_subplot(111, projection="3d")  # 3D-проекция
+# кривые
+ax.plot(r1_sol[:, 0], r1_sol[:, 1], r1_sol[:, 2], color="b")
+ax.plot(r2_sol[:, 0], r2_sol[:, 1], r2_sol[:, 2], color="r")
+# точки останова
+ax.scatter(r1_sol[-1, 0], r1_sol[-1, 1], r1_sol[-1, 2], color="b", label="частица 1")
+ax.scatter(r2_sol[-1, 0], r2_sol[-1, 1], r2_sol[-1, 2], color="r", label="частица 2")
+# подписи
+ax.set_xlabel("x", fontsize=14)  # ось x
+ax.set_ylabel("y", fontsize=14)  # ось y
+ax.set_zlabel("z", fontsize=14)  # ось z
+ax.set_title("Кулоновское взаимодействие электронов\nметод частица-частица")  # заголовок
+ax.legend(loc="upper left")  # легенда
 
-x2 = np.array([x[0] for x in Xj])
-y2 = np.array([x[1] for x in Xj])
-z2 = np.array([x[2] for x in Xj])
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x1[0], y1[0], z1[0], label='x_i', color='b')
-ax.plot(x1, y1, z1, label='x_i', color='b')
-ax.scatter(x2[0], y2[0], z2[0], label='x_j', color='r')
-ax.plot(x2, y2, z2, label='x_j', color='r')
 plt.show()
