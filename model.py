61579a2b0f1fb32ddf31bfe0b7ccbd9f05ae67e0
El'dar Garipov <e_garipow@mail.ru>
2023-05-22 17:51:07 +0300

    В уравнения добавлена масса частиц

--- model.py
+++ model.py
@@ -8,18 +8,18 @@ import scipy
 import matplotlib.pyplot as plt
 
 
-t = 0  # начальное состояние - нулевое время
-T = 0.05  # конечное время
+t0 = 0  # начальное состояние - нулевое время
+Tend = 0.0025  # конечное время
 N = 1000  # количество шагов интегрирования
 
 # частицы
 r1 = [0.0, 0.0, 0.0]  # начальные координаты в м
-r2 = [0.0001, 0.0001, 0.0001]
+r2 = [0.1, 0.1, 0.1]
 r1 = np.array(r1, dtype="float64")  # перевод в np.array
 r2 = np.array(r2, dtype="float64")
 
-v1 = [0.002, 0.001, 0.003]  # начальная скорость в м/с
-v2 = [-0.003, -0.003, -0.001]
+v1 = [40.0, 50.0, 30.0]  # начальная скорость в м/с
+v2 = [-30.0, -50.0, -60.0]
 v1 = np.array(v1, dtype="float64")  # перевод в np.array
 v2 = np.array(v2, dtype="float64")
 
@@ -34,17 +34,17 @@ m0 = 4 * np.pi * 10**(-7)  # магнитная постоянная (Гн/м)
 e0 = 1/(m0 * c**2)  # электричекая постоянная
 k = 1/(4 * np.pi * e0)  # диэлектрическая проницаемость среды
 
-def Equations(params, t, k, q1, q2):
+def CoulombsEquations(params, t, k, q1, q2):
     r1 = params[0:3]  # координаты частицы
     r2 = params[3:6]
     v1 = params[6:9]  # вектор скорости частицы
     v2 = params[9:12]
 
     r12 = np.linalg.norm(r2 - r1)  # расстояние между частицами
-    dv1 = k*q2*(r2 - r1) / r12**3  # вектор приращения скорости
-    dv2 = k*q1*(r1 - r2) / r12**3
-    dr1 = v1  # вектор приращения пути
-    dr2 = v2
+    dv1 = k*q2*q1*(r1 - r2) / (m1 * r12**3) # вектор приращения скорости
+    dv2 = k*q1*q2*(r2 - r1) / (m2 * r12**3)
+    dr1 = v1   # вектор приращения пути
+    dr2 = v2 
 
     r_derivs = np.concatenate((dr1, dr2))
     v_derivs = np.concatenate((dv1, dv2))
@@ -54,8 +54,8 @@ def Equations(params, t, k, q1, q2):
 
 params = np.array([r1, r2, v1, v2])
 params = params.flatten()
-time_arr = np.linspace(0, 0.05, 1001)  # шаги по времени: старт, стоп, количество шагов
-sol = scipy.integrate.odeint(Equations,  # уравнения движения частиц
+time_arr = np.linspace(t0, Tend, N)  # шаги по времени: старт, стоп, количество шагов
+sol = scipy.integrate.odeint(CoulombsEquations,  # уравнения движения частиц
                            params,  # основные параметры (координаты и скорости)
                            time_arr,  # шаги по времени
                            args=(k, q1, q2))  # дополнительные параметры
@@ -63,19 +63,21 @@ r1_sol = sol[:,:3]
 r2_sol = sol[:,3:6]
 
 # график
-fig = plt.figure(figsize=(7, 7))  # размер полотна
+fig = plt.figure("Численная модель", figsize=(6, 6))  # размер полотна
 ax = fig.add_subplot(111, projection="3d")  # 3D-проекция
 # кривые движения частиц
 ax.plot(r1_sol[:, 0], r1_sol[:, 1], r1_sol[:, 2], color="b")
 ax.plot(r2_sol[:, 0], r2_sol[:, 1], r2_sol[:, 2], color="r")
 # точки останова частиц
-ax.scatter(r1_sol[-1, 0], r1_sol[-1, 1], r1_sol[-1, 2], color="b", label="частица 1")
-ax.scatter(r2_sol[-1, 0], r2_sol[-1, 1], r2_sol[-1, 2], color="r", label="частица 2")
+ax.scatter(r1_sol[-1, 0], r1_sol[-1, 1], r1_sol[-1, 2],
+           color="b", label=f"Частица 1, v={round(np.linalg.norm(v1), 2)} м/с")
+ax.scatter(r2_sol[-1, 0], r2_sol[-1, 1], r2_sol[-1, 2],
+           color="r", label=f"Частица 2, v={round(np.linalg.norm(v2), 2)} м/с")
 # подписи
+ax.set_title("Кулоновское взаимодействие двух частиц (электронов)\nметод частица-частица")  # заголовок
+ax.legend(loc="upper left", title=f"t={Tend}")  # легенда
 ax.set_xlabel("x", fontsize=14)  # ось x
 ax.set_ylabel("y", fontsize=14)  # ось y
 ax.set_zlabel("z", fontsize=14)  # ось z
-ax.set_title("Кулоновское взаимодействие электронов\nметод частица-частица")  # заголовок
-ax.legend(loc="upper left")  # легенда
 
 plt.show()
